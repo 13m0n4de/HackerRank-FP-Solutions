33d4b3686fe4f57e88dff1fd806cffe3409de3b3
Yeol <136407746+13m0n4de@users.noreply.github.com>
2024-06-12 12:53:54 +0800

    feat: add Evaluating e^x solution

---
+++ src/11_eval-ex.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+;; factorial : Integer -> Integer
+(define/contract (factorial n)
+  (-> integer? integer?)
+  (let factorial-helper ([acc 1] [n n])
+    (if (= n 0)
+        acc
+        (factorial-helper (* acc n) (- n 1)))))
+
+;; eval-ex : Real -> Real
+(define/contract (eval-ex x)
+  (-> real? real?)
+  (let eval-ex-helper ([acc 0] [y 0])
+    (if (= y 10)
+        acc
+        (eval-ex-helper
+         (+ acc (/ (expt x y) (factorial y)))
+         (+ y 1)))))
+
+(define (read-list)
+  (let read-list-helper ([acc '()])
+    (let ([x (read)])
+      (if (eof-object? x)
+          (reverse acc)
+          (read-list-helper (cons x acc))))))
+
+(let* ([input (read-list)]
+       [lst (cdr input)])
+  (for-each (lambda (x)
+              (displayln (eval-ex x)))
+            lst))
